0af5f2023966e543d31270d02a3ada4453e9a0a6
Jessé <linspw@hotmail.com>
2019-05-29 01:16:31 -0300

    Oitavo commit

--- src/jogopalito/TelaAposta.java
+++ src/jogopalito/TelaAposta.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 public class TelaAposta extends JPanel {
     CardSwitcher card;
+    CardLayout lyt;
     JButton btnProximo;
     JPanel areaAposta, areaTools;
     JLabel frase, lblNumPalitos;
@@ -21,31 +22,35 @@ public class TelaAposta extends JPanel {
         areaTools = new JPanel();
         valor = String.valueOf(data.core.quantJogadores);
         
-        frase = new JLabel("Fase 2");
+        //frase = new JLabel("Fase 2");
         lblNumPalitos = new JLabel();
-        btnProximo = new JButton("Começar");
+        //btnProximo = new JButton("Começar");
         
-        areaAposta.setBackground(Color.red);
         
         areaAposta.setAlignmentX(Component.CENTER_ALIGNMENT);
-        frase.setAlignmentX(Component.CENTER_ALIGNMENT);
+        //frase.setAlignmentX(Component.CENTER_ALIGNMENT);
         lblNumPalitos.setAlignmentX(Component.CENTER_ALIGNMENT);
         
         areaTools.add(lblNumPalitos);
-        areaTools.add(frase);
+        //areaTools.add(frase);
         areaTools.add(areaAposta);
 
-        areaTools.add(btnProximo);
+        //areaTools.add(btnProximo);
         areaTools.setLayout(new BoxLayout(areaTools, BoxLayout.Y_AXIS));
         areaTools.setBackground(Color.CYAN);
         
+        lyt = new CardLayout();
+        areaAposta.setBackground(Color.red);
+        //areaAposta.setLayout(new BoxLayout(areaAposta, BoxLayout.Y_AXIS));
+        areaAposta.setLayout(lyt);
+        
         add(areaTools);
         setVisible(false);
-        btnProximo.addActionListener(new ActionListener() { 
+        /*btnProximo.addActionListener(new ActionListener() { 
             public void actionPerformed(ActionEvent e) { 
                 card.layout.next(card.container);
             } 
-        });
+        });*/
         addComponentListener ( new ComponentAdapter ()
         {
             public void componentShown ( ComponentEvent e )
@@ -60,17 +65,49 @@ public class TelaAposta extends JPanel {
             }
             public void componentHidden ( ComponentEvent e )
             {
+                areaAposta.removeAll();
+                areaAposta.revalidate();
                 System.out.println ( "Hidden TelaAposta Panel" );
             }
         });
     }
     public void apostas(ArrayList<Jogador> listaJogadores){
         for(Jogador e: listaJogadores){
-            areaAposta.removeAll();
-            areaAposta.revalidate();
+            //areaAposta.removeAll();
+            //areaAposta.revalidate();
+            JPanel slotAposta = new JPanel();
             JLabel jogadorNome = new JLabel(e.getNome());
-            areaAposta.add(jogadorNome);
-            JOptionPane.showMessageDialog(null, e.getNome());
+            JTextField txtAposta = new JTextField();
+            JButton enviarAposta = new JButton("Apostar!");
+            
+            slotAposta.setLayout(new BoxLayout(slotAposta, BoxLayout.Y_AXIS));
+            
+            slotAposta.add(jogadorNome);
+            slotAposta.add(txtAposta);
+            slotAposta.add(enviarAposta);
+            //Thread a = new Thread();
+            //a.start();
+            
+            enviarAposta.addActionListener(new ActionListener() { 
+                public void actionPerformed(ActionEvent a) { 
+                    e.setAposta(Float.parseFloat(txtAposta.getText()));
+                    JOptionPane.showMessageDialog(null, e.getNome());
+                    lyt.next(areaAposta);
+                } 
+            });
+            areaAposta.add(slotAposta);
+            //JOptionPane.showMessageDialog(null, e.getNome());
         }
+        JPanel slotAposta = new JPanel();
+        JButton enviarAposta = new JButton("Visualizar Score:!");
+        enviarAposta.addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) { 
+                card.layout.next(card.container);
+            } 
+        });
+        
+        
+        slotAposta.add(enviarAposta);
+        areaAposta.add(slotAposta);
     }
 }
